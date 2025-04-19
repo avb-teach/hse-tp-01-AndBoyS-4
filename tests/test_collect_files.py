85c84455fa65abfcd48b5d30b13e5b4593d4d68e
AndBoyS <andboyxd@gmail.com>
2025-04-19 14:15:55 +0300

    several fixes, --max_depth task

--- tests/test_collect_files.py
+++ tests/test_collect_files.py
@@ -1,10 +1,8 @@
 import subprocess
 import tempfile
-from collections import Counter
-from collections.abc import Generator
+from collections import Counter, defaultdict
 from pathlib import Path
-
-import pytest
+from typing import NamedTuple
 
 from scripts.create_test_data import create_test_structure
 
@@ -12,60 +10,101 @@ REPO_DIR = Path(subprocess.check_output(["git", "rev-parse", "--show-toplevel"])
 SCRIPT_PATH = REPO_DIR / "collect_files.sh"
 
 
-def run_script(input_dir: Path, output_dir: Path) -> None:
-    subprocess.run([str(SCRIPT_PATH), str(input_dir), str(output_dir)], capture_output=True, text=True, check=True)
+def run_script(input_dir: Path, output_dir: Path, max_depth: int | None) -> None:
+    from scripts.collect_files import collect_files
+
+    args = [str(SCRIPT_PATH), str(input_dir), str(output_dir)]
+    if max_depth is not None:
+        args.append("--max_depth")
+        args.append(str(max_depth))
+    collect_files(input_dir, output_dir, max_depth)
 
 
-def get_all_file_names_flat(root_dir: Path) -> list[str]:
-    return sorted([f.name for f in root_dir.iterdir() if f.is_file()])
+class FileData(NamedTuple):
+    path: Path
+    depth: int
 
 
-def get_all_file_names_recursive(root_dir: Path) -> list[str]:
-    return sorted([f.name for f in root_dir.rglob("*") if f.is_file()])
+def get_all_files_info(root_dir: str | Path) -> list[FileData]:
+    result: list[FileData] = []
+    root_path = Path(root_dir).resolve()
+    for p in root_path.rglob("*"):
+        if p.is_file():
+            depth = len(p.relative_to(root_path).parents)
+            result.append(FileData(path=p, depth=depth))
+    return result
 
 
-@pytest.fixture
-def temp_dirs() -> Generator[tuple[Path, Path], None, None]:
+def test_script_runs_and_accepts_two_args() -> None:
     with tempfile.TemporaryDirectory() as input_tmp, tempfile.TemporaryDirectory() as output_tmp:
-        yield Path(input_tmp), Path(output_tmp)
+        input_dir, output_dir = Path(input_tmp), Path(output_tmp)
+        run_script(input_dir, output_dir, max_depth=None)
 
 
-def test_script_runs_and_accepts_two_args(temp_dirs: tuple[Path, Path]) -> None:
-    input_dir, output_dir = temp_dirs
-    run_script(input_dir, output_dir)
+def test_collect_files_depth_2() -> None:
+    _test_collect_files(depth=2)
 
 
-def test_collect_files_depth_2(temp_dirs: tuple[Path, Path]) -> None:
-    _test_collect_files(temp_dirs, depth=2)
-
-
-def test_collect_files_deep(temp_dirs: tuple[Path, Path]) -> None:
+def test_collect_files_deep() -> None:
     for depth in range(3, 6):
-        _test_collect_files(temp_dirs, depth=depth)
+        _test_collect_files(depth=depth)
 
 
-def test_collect_files_non_unique(temp_dirs: tuple[Path, Path]) -> None:
+def test_collect_files_non_unique() -> None:
     for depth in range(3, 6):
-        _test_collect_files(temp_dirs, depth=depth, num_unique_files=5)
+        _test_collect_files(depth=depth, num_unique_files=5)
 
 
-def _test_collect_files(temp_dirs: tuple[Path, Path], depth: int, num_unique_files: int | None = None) -> None:
-    input_dir, output_dir = temp_dirs
+def test_collect_files_max_depth() -> None:
+    for depth in range(3, 6):
+        for max_depth in range(2, 4):
+            _test_collect_files(depth=depth, max_depth=max_depth)
+
+
+def _test_collect_files(
+    depth: int,
+    num_unique_files: int | None = None,
+    max_depth: int | None = None,
+) -> None:
+    assert not all([num_unique_files is not None, max_depth is not None])
     for i in range(1, 6):
-        create_test_structure(
-            input_dir, max_depth=depth, files_per_dir=i, dirs_per_dir=i, num_unique_names=num_unique_files
-        )
-        expected_files = get_all_file_names_recursive(input_dir)
-        run_script(input_dir, output_dir)
-        copied_files = get_all_file_names_flat(output_dir)
-        if num_unique_files is None:
-            assert set(copied_files) == set(expected_files)
-        else:
-            fnames_counter = Counter(expected_files)
-            unique_expected_files = [f for f, count in fnames_counter.items() if count == 1]
-            assert not set(unique_expected_files) - set(copied_files)
-            non_unique_expected_files = [f for f, count in fnames_counter.items() if count > 1]
-            for fname in copied_files:
-                if fname in unique_expected_files:
-                    continue
-                assert any(f.startswith(fname) for f in non_unique_expected_files)
+        with tempfile.TemporaryDirectory() as input_tmp, tempfile.TemporaryDirectory() as output_tmp:
+            input_dir, output_dir = Path(input_tmp), Path(output_tmp)
+            create_test_structure(
+                input_dir, depth=depth, files_per_dir=i, dirs_per_dir=i, num_unique_names=num_unique_files
+            )
+            expected_files = get_all_files_info(input_dir)
+            expected_fnames = [f.path.name for f in expected_files]
+
+            run_script(input_dir, output_dir, max_depth=max_depth)
+            copied_files = get_all_files_info(output_dir)
+            copied_fnames = [f.path.name for f in expected_files]
+
+            if max_depth is not None:
+                _check_max_depth_files(expected_files, copied_files, depth=max_depth)
+            elif num_unique_files is not None:
+                _check_non_unique_files(expected_fnames, copied_fnames)
+            else:
+                assert {p.path.name for p in expected_files} == {p.path.name for p in copied_files}
+
+
+def _check_non_unique_files(expected_fnames: list[str], copied_fnames: list[str]) -> None:
+    fnames_counter = Counter(expected_fnames)
+    unique_expected_fnames = [f for f, count in fnames_counter.items() if count == 1]
+    assert not set(unique_expected_fnames) - set(copied_fnames)
+    non_unique_expected_files = [f for f, count in fnames_counter.items() if count > 1]
+    for fname in copied_fnames:
+        if fname in unique_expected_fnames:
+            continue
+        assert any(f.startswith(fname) for f in non_unique_expected_files)
+
+
+def _check_max_depth_files(expected_files: list[FileData], copied_files: list[FileData], depth: int) -> None:
+    depth_to_expected_fnames: dict[int, set[str]] = defaultdict(set)
+    for f in expected_files:
+        expected_depth = min(f.depth, depth)
+        depth_to_expected_fnames[expected_depth].add(f.path.name)
+    depth_to_copied_fnames: dict[int, set[str]] = defaultdict(set)
+    for f in copied_files:
+        depth_to_copied_fnames[f.depth].add(f.path.name)
+    assert depth_to_expected_fnames == depth_to_copied_fnames
